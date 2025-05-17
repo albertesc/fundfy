a7a0082d8bee33bc6c708a4f1a811da1f7a7ac61
ext-aescamilla <ext-aescamilla@cide.net>
2025-05-17 18:34:53 +0200

    delete .vercel

--- .vercel/output/static/_astro/Hero.astro_astro_type_script_index_0_lang.AXs5FbQM.js
+++
@@ -1,1 +0,0 @@
-import{g as e,S as t}from"./ScrollTrigger.FdPegQMx.js";e.registerPlugin(t);e.set(".hero-text-1",{opacity:1});e.set(".hero-text-2",{display:"none"});e.set(".hero-text-3",{display:"none"});
