cc62a7a90e77df002bc9377b3019b602177e40a1
ext-aescamilla <ext-aescamilla@cide.net>
2025-05-17 16:20:36 +0200

    fix: resolve build error

---
+++ .vercel/output/static/_astro/Hero.astro_astro_type_script_index_0_lang.AXs5FbQM.js
@@ -0,0 +1,1 @@
+import{g as e,S as t}from"./ScrollTrigger.FdPegQMx.js";e.registerPlugin(t);e.set(".hero-text-1",{opacity:1});e.set(".hero-text-2",{display:"none"});e.set(".hero-text-3",{display:"none"});
