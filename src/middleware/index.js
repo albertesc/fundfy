52f11565e35e86539925060b5cc7cd1b59d88464
ext-aescamilla <ext-aescamilla@cide.net>
2025-06-02 18:23:01 +0200

    fix redirection

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,14 +3,16 @@ import { defineMiddleware } from "astro/middleware";
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
 
-  if (url.pathname.startsWith("/es")){
+  if (url.pathname.startsWith("/es")) {
     return next();
   }
 
   const acceptLang = context.request.headers.get("accept-language") || "";
   const userLang = acceptLang.split(",")[0].toLowerCase();
 
-  if (userLang.startsWith("es")) {
+  if (userLang.startsWith("es") && url.pathname === "/") {
     return Response.redirect(new URL("/es", url), 302);
   }
+
+  return next();
 });
