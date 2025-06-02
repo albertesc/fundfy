cce3eac63897269a8cbe84d44f4ec57bdeb42e28
ext-aescamilla <ext-aescamilla@cide.net>
2025-06-02 18:14:55 +0200

    fix redirection on english lang

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,20 +3,14 @@ import { defineMiddleware } from "astro/middleware";
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
 
-  // Si ya está en un idioma específico, continúa normal
   if (url.pathname.startsWith("/es")){
     return next();
   }
 
-  // Detecta idioma del navegador
   const acceptLang = context.request.headers.get("accept-language") || "";
   const userLang = acceptLang.split(",")[0].toLowerCase();
 
-  // Redirige si el idioma es español
   if (userLang.startsWith("es")) {
     return Response.redirect(new URL("/es", url), 302);
   }
-
-  // Redirige por defecto a inglés
-  return Response.redirect(new URL("/", url), 302);
 });
